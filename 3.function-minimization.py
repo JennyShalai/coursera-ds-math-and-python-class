c7f52c725f37f31adb2c3bbe200a5ab266dde741
Jenny <jshalai@intrepid.io>
2020-12-10 11:13:45 -0500

    Update and rename 4.function-minimization.py to 3.function-minimization.py
    fixing weeks naming

--- 3.function-minimization.py
+++ 3.function-minimization.py
@@ -11,7 +11,7 @@ xp = np.linspace(0,30,100)
 _ = plt.plot( xp, f(xp), '-')
 plt.show()
 
-
+# task 1
 task1_1 = minimize(f, 5)
 print(task1_1.fun)
 
@@ -21,17 +21,14 @@ print(task1_2.fun)
 task1_3 = minimize(f, 30, method = 'BFGS')
 print(task1_3.fun)
 
-
-
+# task 2
 from scipy.optimize import differential_evolution
 
 bounds = [(1, 30)]
 task2_1 = differential_evolution(f, bounds)
 print(task2_1)
 
-
-
-
+# task 3
 def f2(x): 
     return int(np.sin(x/5) * np.exp(x/10) + 5*np.exp(-x/2))
 
